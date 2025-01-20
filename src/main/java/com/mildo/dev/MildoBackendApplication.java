8cffed6625fad00ff46205c8e695e3bf069d564b
seop-h <jshwang9709@gmail.com>
2025-01-20 13:19:39 +0900

    initial commit

---
+++ src/main/java/com/mildo/dev/MildoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mildo.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MildoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MildoBackendApplication.class, args);
+    }
+
+}
