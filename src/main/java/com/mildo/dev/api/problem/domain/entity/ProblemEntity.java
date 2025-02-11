fa5ca7870e7aef67211e632eb0f73b8530cfdbf4
Woo-JonHo <jonghowoo33@gmail.com>
2025-02-11 17:04:43 +0900

    [FEAT] Controller Merge 테스트

--- src/main/java/com/mildo/dev/api/problem/domain/entity/ProblemEntity.java
+++ src/main/java/com/mildo/dev/api/problem/domain/entity/ProblemEntity.java
@@ -46,6 +46,9 @@ public class ProblemEntity {
     @Column(name = "problem_type")
     private String problemType;
 
+    @Column(name = "problemd_")
+    private String problemId;
+
     @OneToMany(mappedBy = "problemEntity")
     private final List<CodeEntity> codeList = new ArrayList<>();
 
