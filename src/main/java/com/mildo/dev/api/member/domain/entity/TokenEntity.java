15dca46c8c9714e017226102c129ffd0cce02a6e
황중섭 <jshwang9709@gmail.com>
2025-02-17 12:21:17 +0900

    [FEAT] MemberEnttiy와 TokenEntity의 일대일 매핑관계를 지연 로딩으로 설정

--- src/main/java/com/mildo/dev/api/member/domain/entity/TokenEntity.java
+++ src/main/java/com/mildo/dev/api/member/domain/entity/TokenEntity.java
@@ -2,6 +2,7 @@ package com.mildo.dev.api.member.domain.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -39,7 +40,7 @@ public class TokenEntity {
     @Column(name = "refresh_expiration_time")
     private Timestamp refreshExpirationTime;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private MemberEntity memberEntity;
 
