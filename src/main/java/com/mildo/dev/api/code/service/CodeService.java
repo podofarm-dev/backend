6e5c5e869e0d377a476a9a4aff445d044401a671
Imdo714 <gusehdla@naver.com>
2025-02-18 13:45:03 +0900

    [CHORE] monitoring 설정

--- src/main/java/com/mildo/dev/api/code/service/CodeService.java
+++ src/main/java/com/mildo/dev/api/code/service/CodeService.java
@@ -86,20 +86,7 @@ public class CodeService {
 
 
 
-//    public List<CommentResponse> allComment(Long codeNo){
-//        checkCode(codeNo);
-//        List<CommentEntity> comments = commentRepository.findByCodeEntity_CodeNo(codeNo);
-//
-//        return comments.stream()
-//                .map(comment -> new CommentResponse(
-//                        comment.getCommentNo(),
-//                        comment.getCommentContent(),
-//                        comment.getCommentDate(),
-//                        comment.getMemberEntity().getMemberId(),
-//                        comment.getCodeEntity().getCodeNo()
-//                ))
-//                .collect(Collectors.toList());
-//    }
+
 
     public CommentListResponse allComment(Long codeNo) {
         CodeEntity code = codeRepository.findByIdWithComments(codeNo)
